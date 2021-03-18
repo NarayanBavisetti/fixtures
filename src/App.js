52290dbd0137f911c64d65ea9a8e859bd13bcff2
narayan <narayan3119@gmail.com>
2021-03-18 23:10:09 +0530

    added firebase auth

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import './App.css';
+
+import Weather from './components/weatherapp'
 
 function App() {
   return (
     <div className="App">
-  Weather app
+ <Weather />
     </div>
   );
 }
